0329a2ac48ef5401a18af22dab8a95096d83276f
SantiagoAjala0204 <91999660+SantiagoAjala0204@users.noreply.github.com>
2025-05-28 20:20:11 -0500

    Change python file

--- inferir.py
+++ inferir.py
@@ -6,7 +6,7 @@ features = [[19, 2, 20, 0, 6,
              False, True, False, False, True, False,
              False, False, False, True, False, False]]
 # Cargar el modelo
-modelo = joblib.load("modelo_random_forest.pkl")
+modelo = joblib.load("modelo_decision_tree.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
